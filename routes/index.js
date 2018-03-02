f62e6717170f95b22c0456c82329d64cccd28787
Samson  Leung <Samson.Leung@stonybrook.edu>
2018-03-02 13:27:14 -0500

    C:/Users/samleung/Desktop/PortableGit/listgames

--- routes/index.js
+++ routes/index.js
@@ -13,45 +13,46 @@ router.get('/ttt', function (req, res, next) {
 // https://medium.freecodecamp.org/requiring-modules-in-node-js-everything-you-need-to-know-e7fbd119be8
 var User = require('../user/User');
 router.post('/ttt', function (req, res, next) {
-	if(!req.body.username) var login = true;
+	if (!req.body.username) var login = true;
 	else login = false;
 	res.render('index', { login });
 });
 
-router.post('/ttt/play', function (req, res, next) {
+router.post('/ttt/play', function tttPost(req, res, next) {
 	var cookie = currentUser._doc;
 	var move = req.body.move;
 
 	console.log('PLAYER:' + cookie.username + ' MOVE:' + move);
 	// if move = null
-	if(move == null){
+	if (move == null) {
 		console.log("MOVE NULL");
 		res.status(200).json({
 			status: 'OK',
-			grid: [' ',' ',' ',' ',' ',' ',' ',' ',' ']
+			grid: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 		});
 		return;
 	}
 
-	User.findOne({username: cookie.username}, function (err, user) {
+	User.findOne({ username: cookie.username }, function makeMove(err, user) {
 		console.log('FOUND: ' + user.username);
 
 		var firstGame = user.games.length == 0;
+		var updateGame = true;
 
-		if(firstGame){
+		if (firstGame) {
 			console.log('First Game!');
 
 			var id = 1;
 			var startDate = Math.floor(new Date() / 1000);
-			var grid = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
+			var grid = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 			grid[move] = 'X';
 			var winner = ' ';
 
 			//add game to games array
-			user.games.push({id: id, start_date: startDate, grid: grid, winner: winner});
+			user.games.push({ id: id, start_date: startDate, grid: grid, winner: winner });
 
 			//update user on database
-			user.save(function (err, updateduser) {
+			user.save(function updateUser(err, updateduser) {
 				if (err) return handleError(err);
 
 				//update cookie
@@ -63,7 +64,43 @@ router.post('/ttt/play', function (req, res, next) {
 					winner: winner
 				});
 			});
+		} else if (updateGame) {
+			console.log("Update Game!");
 
+			var grid = user.games[0].grid;
+			grid[move] = 'X';
+
+			User.update(
+				{'games.id': 1},
+				{ $set: {'games.$.grid': grid } },
+				function updateGrid(err, result) {
+					if (err) console.log(handleError(err));
+
+					res.status(200).json({
+						status: 'OK',
+						grid: user.games[0].grid,
+						winner: user.games[0].winner
+					});
+				}
+			);
+
+
+			// //update grid array in game
+			// user.games[0].grid[move] = 'X';
+
+			// //update user on database
+			// user.save(function updateUser(err, updateduser) {
+			// 	if (err) return handleError(err);
+
+			// 	//update cookie
+			// 	currentUser = Object.assign({}, updateduser);
+			// 	//return status and grid
+			// 	res.status(200).json({
+			// 		status: 'OK',
+			// 		grid: user.games[0].grid,
+			// 		winner: user.games[0].winner
+			// 	});
+			// });
 		}
 	});
 });
@@ -96,7 +133,7 @@ router.post('/verify', function (req, res, next) {
 	var email = req.body.email;
 	var key = req.body.key;
 	var backDoor = 'abracadabra';
-	
+
 	//console.log("email: " + email + "\nkey: " + key);
 	User.findOne({ email: email }, function (err, user) {
 		if (err || !user) {
@@ -118,9 +155,9 @@ router.post('/verify', function (req, res, next) {
 					res.status(200).json({
 						status: 'OK'
 					});
-				  });
+				});
 
-			}else{
+			} else {
 				res.status(200).json({
 					status: 'ERROR'
 				});
@@ -141,7 +178,7 @@ router.post('/login', function (req, res, next) {
 		} else {
 			if ((username == user.username) && (password == user.password) && user.verified) {
 				console.log(user.username + ' LOGIN!');
-				
+
 				//add cookie
 				currentUser = Object.assign({}, user);
 				res.json({
@@ -180,7 +217,7 @@ router.post('/getgame', function (req, res, next) {
 
 router.post('/getscore', function (req, res, next) {
 	res.status(200).json({
-		status: 'OK'	
+		status: 'OK'
 	});
 });
 
@@ -188,45 +225,45 @@ router.post('/getscore', function (req, res, next) {
 function makeKey() {
 	var text = "";
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  
+
 	for (var i = 0; i < 5; i++)
-	  text += possible.charAt(Math.floor(Math.random() * possible.length));
-  
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+
 	return text;
 }
 
-function checkWinner(grid){
+function checkWinner(grid) {
 	if (
 		// HORIZONTAL WINS
-	   (grid[0] == 'X' && grid[1] =='X' && grid[2] == 'X') ||
-	   (grid[3] == 'X' && grid[4] =='X' && grid[5] == 'X') ||
-	   (grid[6] == 'X' && grid[7] =='X' && grid[8] == 'X') ||
+		(grid[0] == 'X' && grid[1] == 'X' && grid[2] == 'X') ||
+		(grid[3] == 'X' && grid[4] == 'X' && grid[5] == 'X') ||
+		(grid[6] == 'X' && grid[7] == 'X' && grid[8] == 'X') ||
 		// DIAGONAL WINS
-	   (grid[0] == 'X' && grid[4] =='X' && grid[8] == 'X') ||
-	   (grid[2] == 'X' && grid[4] =='X' && grid[6] == 'X') ||
+		(grid[0] == 'X' && grid[4] == 'X' && grid[8] == 'X') ||
+		(grid[2] == 'X' && grid[4] == 'X' && grid[6] == 'X') ||
 		// VERTICAL WINS
-	   (grid[0] == 'X' && grid[3] =='X' && grid[6] == 'X') ||
-	   (grid[1] == 'X' && grid[4] =='X' && grid[7] == 'X') ||
-	   (grid[2] == 'X' && grid[5] =='X' && grid[8] == 'X')
-	 ) {
+		(grid[0] == 'X' && grid[3] == 'X' && grid[6] == 'X') ||
+		(grid[1] == 'X' && grid[4] == 'X' && grid[7] == 'X') ||
+		(grid[2] == 'X' && grid[5] == 'X' && grid[8] == 'X')
+	) {
 		return 'X'; //return X as winner
-	  }
-		// IF 'O' WINS
-	  else if (
+	}
+	// IF 'O' WINS
+	else if (
 		// HORIZONTAL WINS
-	   (grid[0] == 'O' && grid[1] =='O' && grid[2] == 'O') ||
-	   (grid[3] == 'O' && grid[4] =='O' && grid[5] == 'O') ||
-	   (grid[6] == 'O' && grid[7] =='O' && grid[8] == 'O') ||
+		(grid[0] == 'O' && grid[1] == 'O' && grid[2] == 'O') ||
+		(grid[3] == 'O' && grid[4] == 'O' && grid[5] == 'O') ||
+		(grid[6] == 'O' && grid[7] == 'O' && grid[8] == 'O') ||
 		//DIAGONAL WINS
-	   (grid[0] == 'O' && grid[4] =='O' && grid[8] == 'O') ||
-	   (grid[2] == 'O' && grid[4] =='O' && grid[6] == 'O') ||
+		(grid[0] == 'O' && grid[4] == 'O' && grid[8] == 'O') ||
+		(grid[2] == 'O' && grid[4] == 'O' && grid[6] == 'O') ||
 		//VERTICAL WINS
-	   (grid[0] == 'O' && grid[3] =='O' && grid[6] == 'O') ||
-	   (grid[1] == 'O' && grid[4] =='O' && grid[7] == 'O') ||
-	   (grid[2] == 'O' && grid[5] =='O' && grid[8] == 'O')
-	 ){
+		(grid[0] == 'O' && grid[3] == 'O' && grid[6] == 'O') ||
+		(grid[1] == 'O' && grid[4] == 'O' && grid[7] == 'O') ||
+		(grid[2] == 'O' && grid[5] == 'O' && grid[8] == 'O')
+	) {
 		return 'O' //return O as winner
-	 }
+	}
 }
 
 module.exports = router;
